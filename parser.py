eb92cef80ad6d8ddf1c3bd5c55f19c04b93300ed
rubcuadra <rubcuadra@gmail.com>
2018-09-21 15:39:17 -0500

    done, expression still bugged

--- parser.py
+++ parser.py
@@ -37,7 +37,7 @@ class Node:
     def printTree(_node,level=0):
         if _node is None: return
         tb = (' |  '*level) + ' |- '
-        print(f"{tb}{_node.type} {_node.value if _node.value else ''}" )
+        print(f"{tb}{_node.type} {_node.value if _node.value!=None else ''}" )
         for c in _node.children:
             Node.printTree(c, level= level+1)
 
@@ -63,18 +63,18 @@ def p_declaration(node_type="declaration"): #Returns a Node
     if match("ID"):
         decName = Node(cT.type, value=cT.value)
         if match("SEMI"):     # ;
-            return Node(node_type, [ts,decName,Node("SEMI")], "VARIABLE")
+            return Node(node_type, [ts,decName], "VARIABLE")
         elif match("LBRACK"):   # [
             cT = token
             iVal = Node(cT.type, value=cT.value) #INTEGER value
             if match("INTEGER") and match("RBRACK") and match("SEMI"):
-                return Node(node_type, [ts,decName,Node("LBRACK"),iVal,Node("RBRACK"),Node("SEMI") ], "ARRAY")
+                return Node(node_type, [ts,decName,Node("LBRACK"),iVal,Node("RBRACK") ], "ARRAY")
         elif match("LPAREN"):   # (
             params = p_params()
             if params and match("RPAREN"):
                 cps = p_compound_stmt()
                 if cps: #!=None
-                    return Node(node_type, [ts,decName,Node("LPAREN"),*params,Node("RPAREN"),cps ], "FUNCTION")
+                    return Node(node_type, [ts,decName,*params,cps ], "FUNCTION")
     p_error()
 
 def p_params(): #Returns a list of Nodes
@@ -128,7 +128,7 @@ def p_compound_stmt(node_type="compound_statements"): #Returns a
         ldl = p_local_declarations_list()
         sl = p_statement_list()
         if match("RCBRACES"):
-            return Node(node_type,[Node("LCBRACES"),*ldl,*sl,Node("RCBRACES")])
+            return Node(node_type,[*ldl,*sl])
     # p_error()
 
 def p_local_declarations_list(node_type = "local_declaration"): #Returns array
@@ -181,6 +181,10 @@ def p_iteration_stmt():
     if match("while"):
         if match("LPAREN"):
             e = p_expression()
+            
+            for c in e.children: 
+                if c == None: raise
+                
             if e and match("RPAREN"):
                 s = p_statement()
                 if s:
@@ -195,7 +199,6 @@ def p_selection_stmt():
     if match("if"):
         if match("LPAREN"):
             e = p_expression() #Can't be None
-            print(e)
             if e and match("RPAREN"):
                 s = p_statement()
                 if match("else"): #Segunda parte
@@ -226,12 +229,12 @@ def p_expression_stmt(node_type="expression_stmt"): #Can return None
                        | SEMI 
     """
     if match("SEMI"):
-        return Node(node_type,[Node("SEMI")]) 
+        return Node("SEMI")
     else:
         e = p_expression()
         if e:
             if match("SEMI"): 
-                return Node(node_type,[e,Node("SEMI")])  
+                return e 
             p_error()
 
 def p_expression(node_type="expression"): 
@@ -306,6 +309,7 @@ def p_expression(node_type="expression"):
     factor = idNode if idNode else p_factor()
     multis = p_multis(factor)
     #AQUI HAY BUGGS
+    print("VAMO")
     if multis: #L es multis
         sumres = p_sumres(multis)
         relop = p_relop()
@@ -341,7 +345,9 @@ def p_expression(node_type="expression"):
                 return sumres
         else: #L is factor
             if relop:
+                print(token)
                 term2 = p_factor()
+                print(term2)
                 sumres2 = p_sumres(term2)
                 if sumres2: relop.children = [factor,sumres2]
                 else:       relop.children = [factor,term2]
@@ -355,6 +361,7 @@ def p_sumres(L): #Puede regresar None
     """
     res = None
     addop = p_addop()
+    if L is None: L = Node("INTEGER",value=0) #For unitary +/-
     while addop:
         addop.children = [L, p_factor()]
         mts = p_multis(addop)
@@ -404,6 +411,7 @@ def p_factor(node_type="factor"):
                 if match("RPAREN"):
                     return Node(node_type,[decName,Node("LPAREN"),*args,Node("RPAREN")])
                 p_error()
+            return decName
 
 def p_mulop(node_type="mulop"):
     if match("TIMES"):  return Node(node_type,[],"TIMES")
@@ -449,12 +457,12 @@ def p_var_declaration(node_type = "declaration"): #Can return None
         if match("ID"):
             decName = Node(cT.type, value=cT.value)
             if match("SEMI"):     # ;
-                return Node(node_type, [ts,decName,Node("SEMI")], "VARIABLE")
+                return Node(node_type, [ts,decName], "VARIABLE")
             elif match("LBRACK"):   # [
                 cT = token
                 iVal = Node(cT.type, value=cT.value) #INTEGER value
                 if match("INTEGER") and match("RBRACK") and match("SEMI"):
-                    return Node(node_type, [ts,decName,Node("LBRACK"),iVal,Node("RBRACK"),Node("SEMI") ], "ARRAY")
+                    return Node(node_type, [ts,decName,Node("LBRACK"),iVal,Node("RBRACK") ], "ARRAY")
             else:
                 p_error()
     
