eb3db3122b290365366b48efef16e471034d401b
rubcuadra <rubcuadra@gmail.com>
2018-10-25 21:33:39 -0500

    missing whiles and ifs

--- semantica.py
+++ semantica.py
@@ -13,16 +13,17 @@ class ScopeTree():
         self.children = []
     
     #dataType, strucType, value
-    def addSymbol(self, s): #Check if it exists before inserting?
+    def addSymbol(self, s): 
+        #Check if it already exists before inserting?
         if s.value == "ARRAY":
             self.scope[s.children[1].value] = (s.children[0].type,defTypes.ARR,s.children[3].value) 
             # for i in range( ch.children[3].value ): st.addSymbol(f'{ch.children[1].value}_{i}',ch.children[0].type) 
         elif s.value == "VAR":
             self.scope[s.children[1].value] = (s.children[0].type,defTypes.VAR,None) #Vars are initialized in None
         elif s.value == "FUNCTION":
-            self.scope[s.children[1].value] = (s.children[0].type,defTypes.FUN,None) #functions are initialized in None
+            self.scope[s.children[1].value] = (s.children[0].type,defTypes.FUN,*s.children[2:-1]) #functions are initialized in None
         else: 
-            raise Exception("Wrong symbol")
+            raise Exception("Wrong declaration")
 
     def getSymbol(self,var): 
         if var in self.scope: return self.scope[var]
@@ -43,20 +44,25 @@ class ScopeTree():
                 if statement.type == 'declaration': #Define it in the scope
                     _scope.addSymbol(statement)
                 elif statement.type == "EQUALS":    #Validate var exist and check that assign is of the same type
-                    s = _scope.getSymbol(statement.children[0].value)
-                    if s:
+                    if statement.children[0].type in ["ARRAY_POS", "ID"]:
+                        s = getType(statement.children[0],_scope)
                         gt = getType(statement.children[1],_scope)
-                        if s[0] == gt: continue
+                        if s == gt: continue
                         raise Exception(f"Wrong data type assignment: {s[0]} {statement.children[0].value} => {gt}")
-                    else: 
-                        raise Exception(f"Var does not exist {statement.children[0].value}")
+                    else:
+                        raise Exception("Wrong assignment")
                 elif statement.type == "CALL":      #Check called func exists and it is void
-                    s = _scope.getSymbol(statement.children[0].value)
-                    if not s:        raise Exception(f"Calling function without definition '{statement.children[0].value}'")
-                    if s[0]!='void': raise Exception(f"Called NON void function without assignment")
+                    s = getType(statement.children[0],_scope)
+                    if not s:     raise Exception(f"Calling function without definition '{statement.children[0].value}'")
+                    if s!='void': raise Exception(f"Called NON void function without assignment")
                 elif statement.type == "return_stmt":  #Check returns exist
-                    pass #TODO
+                    s = _scope.getSymbol(_scope.tag) #SI es None es que we fucked up algo
+                    if s[0] == 'void' and len(statement.children)>1: raise Exception("ERROR: void function returning values")
+                    t = getType(statement.children[1],_scope)
+                    if s[0] == t: continue
+                    raise Exception(f"Wrong return type {s[0]} != {t}")
                 elif statement.type in ["while","if","else"]: #Recursivo
+                    print(statement)
                     pass #TODO
                 else: 
                     raise Exception(f"unkown type {statement.type}")
@@ -76,11 +82,13 @@ class ScopeTree():
 def getType(parseNode,scopeNode):
     if parseNode.type == 'ID':
         c = scopeNode.getSymbol(parseNode.value)
-        return c[0]
+        if c: return c[0]
+        else: raise Exception(f"undefined variable {parseNode.value}")
     elif parseNode.type == "ARRAY_POS": 
         c = scopeNode.getSymbol(parseNode.children[0].value)
         #TODO parseNode.children[2] can be an expression with equals, check types??
-        return c[0]
+        if c: return c[0]
+        else: raise Exception(f"undefined variable {parseNode.children[0].value}")
     elif parseNode.type == "INTEGER":  
         return 'int' 
     else: #Opps
@@ -96,11 +104,19 @@ def getType(parseNode,scopeNode):
             raise Exception("Wrong types on mulop")
         elif parseNode.type == 'CALL':
             c = getType(parseNode.children[0],scopeNode)
-            if len(parseNode.children)>1: print("TODO",c) #TODO validate params exist
+            if len(parseNode.children)>1: 
+                f = scopeNode.getSymbol(parseNode.children[0].value)
+                for i,passedParam in enumerate(parseNode.children[1:], start=2):
+                    if getType(f[i],scopeNode) != getType(passedParam,scopeNode): 
+                        raise Exception(f"Wrong param type: function {parseNode.children[0].value} param #{start-1}")
             return c
+        elif parseNode.type == 'param':
+            if parseNode.value   == 'ARRAY': return parseNode.children[0].type
+            elif parseNode.value == 'VAR':   return parseNode.children[0].type
+            else:           raise Exception("unexpected param type",parseNode)
         else:
             print('TODO',parseNode.type, parseNode.value)
-    return 'int'
+    # return 'int'
 
 #Recibe como argumento el resultado de la funcion parser() en el archivo parser.py
 #Regresa la tabla de simbolos
@@ -116,7 +132,7 @@ def tabla(node, imprime = True):
     else:
         raise Exception("error, program starts with", ch.type)
 
-    for c in st.children: print(c)
+    # for c in st.children: print(c)
     
 
 #Recibe como argumento el resultado de la funcion parser() en el archivo parser.py
@@ -135,7 +151,7 @@ if __name__ == '__main__':
     # Segundo Parcial
     from globalTypes import *
     from parser import parser, globales
-    f = open('examples/3.c-', 'r')
+    f = open('examples/2.c-', 'r')
     programa = f.read()
     progLong = len(programa)
     programa = programa + TokenType.ENDFILE.value
